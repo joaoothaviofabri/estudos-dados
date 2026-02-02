c1646342b8f8dda5668c5d6978ffd75e84795b89
João Othávio <joaoothaviofabri14@gmail.com>
2026-02-01 21:54:18 -0300

    Adicionando o projeto de informações e filtro de carros. (Incompleto).

--- projetos/streamlit/concessionaria/concessionaria.py
+++ projetos/streamlit/concessionaria/concessionaria.py
@@ -4,8 +4,21 @@ import pandas as pd
 import numpy as np
 
 #carregamento de dados
-df = pd.read_csv('carros.csv')
+dados = pd.read_csv('carros.csv')
+dados_marcas = list(dados['marca'].drop_duplicates())
+quantidade_carros_por_marca = dados.groupby('marca')['marca'].count()
 
-st.title("Informações Técnicas de Carros - Estoque de Concessionária")
+st.write("""
+# Informações Técnicas de Carros
+""")
 
+#  Vizualização - Filtros
 st.sidebar.header("Filtros")
+
+# Gráfico 
+st.bar_chart(quantidade_carros_por_marca)
+
+# Filtro de vizualização
+selecao_marcas = st.sidebar.multiselect('Selecione a marca para vizualizar', dados_marcas)
+
+selecao_info_carro = st.sidebar.multiselect('Selecione a informação que deseje vizualizar', dados_marcas)
